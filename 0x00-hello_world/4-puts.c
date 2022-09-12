a457124e9542f52567b9c275bae55ecf7d321795
root <chibuzosampson7@gmail.com>
2022-09-12 02:22:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * * main - prints the size of various types
+ *main - prints the size of various types
  *
- * * Return: 0 if exited properly, non-zero otherwise
+ *Return: 0 if exited properly, non-zero otherwise
  *
  *
  */
